6ba3e94c739164d4c7ecbda0a54f3f625a3df28d
Adam Gibbs <36103496+venomz75@users.noreply.github.com>
2019-09-20 19:26:07 +0100

    weaponBar: removed redundancies + added ordering
    -Uses a loop to print the elements now that I am happy with the layout.
    -Allows for custom ordering of weapons instead of being restricted to the default order of other HUDs.

--- weaponBar.lua
+++ weaponBar.lua
@@ -1,116 +1,37 @@
 require "base/internal/ui/reflexcore"
-
-weaponBar = {}
-registerWidget("weaponBar")
+weaponBar = {}; registerWidget("weaponBar")
 
 function weaponBar:draw()
     if not shouldShowHUD() then return end
-        player = getPlayer()
+    player = getPlayer()
         if  player ~= nil then
-            VMHUD_FONT = "RobotoMono-Bold"
-            x = 40
-            iconY = 400
-            textY = 350
-            colourBlack = Color(0,0,0,255)
-            meleeItem = inventoryDefinitions[player.inventoryMelee]
-        
-            --Rocket
-            rocketX = -280
-            if player.weaponIndexSelected == 6 then
-                weaponSelected(rocketX)
+            --init
+            VMHUD_FONT = "RobotoMono-Bold"; iconY = 400; textY = 350; colourBlack = Color(0,0,0,255)
+            svgFilepath = "internal/ui/icons/weapon"; meleePath = inventoryDefinitions[player.inventoryMelee].asset;
+            positions = {{6,-280},{7,-200},{8,-120},{3,-40},{4,40},{5,120},{2,200},{1,280}}
+            nvgFontSize(40); nvgFontFace(VMHUD_FONT); nvgTextAlign(NVG_ALIGN_CENTER, NVG_ALIGN_MIDDLE)
+            --main loop
+            for i = 1, 8 do
+                x = positions[i][2]
+                wep = positions[i][1] 
+                --highlight selected weapon
+                if player.weaponIndexSelected == wep then
+                    weaponSelected(x)
+                end
+                --if melee, use its own skin icon
+                if wep == 1 then
+                    filepath = meleePath
+                else
+                    filepath = svgFilepath..wep
+                end
+                --drop shadow       
+                nvgFillColor(colourBlack)
+                nvgSvg(filepath,x+2,iconY+2,30)
+                --icon
+                nvgFillColor(player.weapons[wep].color)
+                if wep ~= 1 then nvgText(x,textY, player.weapons[wep].ammo) end
+                nvgSvg(filepath,x-2,iconY-2,30)       
             end
-            nvgFontSize(40)
-            nvgFontFace(VMHUD_FONT)
-            nvgTextAlign(NVG_ALIGN_CENTER, NVG_ALIGN_MIDDLE)
-            nvgFillColor(colourBlack)
-            nvgSvg("internal/ui/icons/weapon6",rocketX+2,iconY+2,30)
-            nvgFillColor(player.weapons[6].color)
-            nvgText(rocketX,textY, player.weapons[6].ammo)
-            nvgSvg("internal/ui/icons/weapon6",rocketX-2,iconY-2,30)
-            
-
-            --Ion
-            ionX = -200
-            if player.weaponIndexSelected == 7 then
-                weaponSelected(ionX)
-            end
-            nvgFillColor(colourBlack)
-            nvgSvg("internal/ui/icons/weapon7",-x*5+2,iconY+2,30)
-            nvgFillColor(player.weapons[7].color)
-            nvgText(-200,textY, player.weapons[7].ammo)
-            nvgSvg("internal/ui/icons/weapon7",-x*5,iconY-2,30)
-            
-
-            --Bolt
-            boltX = -120
-            if player.weaponIndexSelected == 8 then
-                weaponSelected(boltX)
-            end
-            nvgFillColor(colourBlack)
-            nvgSvg("internal/ui/icons/weapon8",-x*3+2,iconY+2,30)
-            nvgFillColor(player.weapons[8].color)
-            nvgText(-120,textY, player.weapons[8].ammo)
-            nvgSvg("internal/ui/icons/weapon8",-x*3-2,iconY-2,30)
-            
-
-            --Shotgun
-            shotgunX = -40
-            if player.weaponIndexSelected == 3 then
-                weaponSelected(shotgunX)
-            end
-            nvgFillColor(colourBlack)
-            nvgSvg("internal/ui/icons/weapon3",-x+2,iconY+2,30)
-            nvgFillColor(player.weapons[3].color)
-            nvgText(-40,textY, player.weapons[3].ammo)
-            nvgSvg("internal/ui/icons/weapon3",-x-2,iconY-2,30)
-            
-
-            --Grenade
-            grenadeX = 40
-            if player.weaponIndexSelected == 4 then
-                weaponSelected(grenadeX)
-            end
-            nvgFillColor(colourBlack)
-            nvgSvg("internal/ui/icons/weapon4",x+2,iconY+2,30)
-            nvgFillColor(player.weapons[4].color)
-            nvgText(40,textY, player.weapons[4].ammo)
-            nvgSvg("internal/ui/icons/weapon4",x-2,iconY-2,30)
-            
-
-            --Plasma
-            plasmaX = 120
-            if player.weaponIndexSelected == 5 then
-                weaponSelected(plasmaX)
-            end
-            nvgFillColor(colourBlack)
-            nvgSvg("internal/ui/icons/weapon5",x*3+2,iconY+2,30)
-            nvgFillColor(player.weapons[5].color)
-            nvgText(120,textY, player.weapons[5].ammo)
-            nvgSvg("internal/ui/icons/weapon5",x*3-2,iconY-2,30)
-            
-            
-            --Burst
-            burstX = 200
-            if player.weaponIndexSelected == 2 then
-                weaponSelected(burstX)
-            end
-            nvgFillColor(colourBlack)
-            nvgSvg("internal/ui/icons/weapon2",x*5+2,iconY+2,30)
-            nvgFillColor(player.weapons[2].color)
-            nvgText(200,textY, player.weapons[2].ammo)
-            nvgSvg("internal/ui/icons/weapon2",x*5-2,iconY-2,30)
-            
-            
-            --Melee
-            meleeX = 280
-            if player.weaponIndexSelected == 1 then
-                weaponSelected(meleeX)
-            end
-            nvgFillColor(colourBlack)
-            nvgSvg(meleeItem.asset,meleeX+2,iconY+2,30)
-            nvgFillColor(player.weapons[1].color)
-            nvgSvg(meleeItem.asset,meleeX-2,iconY-2,30)
-
     end
 end
 
